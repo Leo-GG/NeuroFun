3175e3cb075f12177de874b1c22e5b9ac5659c56
LeoGG <you@example.com>
2017-02-28 13:21:48 +0100

    Updatede demo script

--- demo.m
+++ demo.m
@@ -1,15 +1,13 @@
 
 %% Load spikes from a raw recording, as detected by the Mea1kServer
-rawFile='/net/bs-filesvr02/export/group/hierlemann/recordings/Mea1k/michelef/170131/data/665/electrode_selection/Method2/000.raw.h5';
-Spike=getSpikesFromRecord(rawFile);
-% Get also pairwise distances
-f=mea1k.file(rawFile);
-dist=squareform(pdist([f.x f.y]));
+load('NeuroFun/demoData/demoSpike.map')
+load('NeuroFun/demoData/mapP.map')
+dist=mapP
 
 %% Restrict to first two minutes 
-demoSpike.C=Spike.C(Spike.T<120);
-demoSpike.T=Spike.T(Spike.T<120);
-demoSpike.A=Spike.A(Spike.T<120);
+demoSpike.C=Spike.C(Spike.T<30);
+demoSpike.T=Spike.T(Spike.T<30);
+demoSpike.A=Spike.A(Spike.T<30);
 
 %% Perform full analysis
 demoChar = neuroFun(demoSpike) 
