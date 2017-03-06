080bb9897215ac8915bd711ea874a37f8943132f
rolandd <roland.diggelmann@bsse.ethz.ch>
2017-03-06 20:02:23 +0100

    Bigfix for loop

--- +basic/getAmplitudes.m
+++ +basic/getAmplitudes.m
@@ -1,8 +1,10 @@
 function spikeAmps = getAmplitudes(Spike)
 % Return mean peak amplitude for each channel. Input contains channel
 % number and amplitude for each spike
-    spikeAmps = zeros(max(Spike.C),1);
-    for i=1:max(Spike.C)
-        spikeAmps(i)=median(Spike.A(Spike.C==i));        
+    c = unique(Spike.C);    
+    spikeAmps = zeros(numel(c),1);
+    
+    for i=1:numel(c)
+        spikeAmps(i)=median(Spike.A(Spike.C==c(i)));        
     end    
 end
